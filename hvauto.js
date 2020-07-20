f0286aec237ba47081d113d7ed9890b200c8de05
nekosu <liao.hy@outlook.com>
2020-07-20 20:02:15 +0800

    Support items.
    Fix health.
    Fix round.
    Add item list.

--- hvauto.js
+++ hvauto.js
@@ -32,6 +32,7 @@ hvauto.requestPage = async function (url) {
 			"sec-fetch-site": "none",
 			"sec-fetch-user": "?1",
 			"upgrade-insecure-requests": "1",
+			"user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.89 Safari/537.36",
 			"cookie": hvcookie
 		},
 		"referrerPolicy": "no-referrer-when-downgrade",
@@ -57,6 +58,7 @@ hvauto.requestJson = async function (body) {
 			"sec-fetch-dest": "empty",
 			"sec-fetch-mode": "cors",
 			"sec-fetch-site": "same-origin",
+			"user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.89 Safari/537.36",
 			"cookie": hvcookie
 		},
 		"referrer": "https://hentaiverse.org/",
@@ -99,59 +101,83 @@ hvauto.init = async function (cookie) {
 	if ($('#mainpane').children('#battle_top').length > 0) {
 		hvauto.battle.token = /var battle_token = "([a-z0-9]+)";/.exec($('#mainpane').children('script').eq(1).html())[1];
 		hvauto.battle.charge = 0;
-		$('#vcp').children('div').children().each(function(i, e) {
+		$('#vcp').children('div').children().each(function (i, e) {
 			if ($(this).attr('id') == 'vcr') {
 				hvauto.battle.charge += 0.5;
 			} else {
 				hvauto.battle.charge += 1;
 			}
 		});
-		hvauto.battle.health = $('#vrhd').text();
+		if ($('#vrhb').length > 0) {
+			hvauto.battle.health = $('#vrhb').text();
+		} else {
+			hvauto.battle.health = $('#vrhd').text(); // This seems used when Spark of Life is active
+		}
 		hvauto.battle.mana = $('#vrm').text();
 		hvauto.battle.spirit = $('#vrs').text();
 		hvauto.battle.skills = {}; // both skills and magic are in it
-		$('#table_skills .bts').children('div').each(function(i, e) {
+		$('#table_skills .bts').children('div').each(function (i, e) {
 			let obj = hvauto.parseSpellScript($(this).attr('onmouseover'));
 			obj.id = Number($(this).attr('id'));
 			obj.available = $(this).attr('style') != 'opacity:0.5';
 			hvauto.battle.skills[obj.name] = obj;
 		});
-		$('#table_magic .bts').children('div').each(function(i, e) {
+		$('#table_magic .bts').children('div').each(function (i, e) {
 			let obj = hvauto.parseSpellScript($(this).attr('onmouseover'));
 			obj.id = Number($(this).attr('id'));
 			obj.available = $(this).attr('style') != 'opacity:0.5';
 			hvauto.battle.skills[obj.name] = obj;
 		});
 		hvauto.battle.effect = [];
-		$('#pane_effects img').each(function(i, e) {
+		$('#pane_effects img').each(function (i, e) {
 			hvauto.battle.effect.push(hvauto.parseEffectScript($(this).attr('onmouseover')));
 		});
+		hvauto.battle.item = {};
+		if ($('#pane_item').children('.bti1').children('.bti3').children().length > 0) {
+			let obj = {
+				'id': 999
+			};
+			obj.name = $('#pane_item').children('.bti1').children('.bti3').children().children().children().text();
+			obj.available = $('#pane_item').children('.bti1').children('.bti3').children().attr('onclick') != undefined;
+			hvauto.battle.item[999] = obj;
+		}
+		$('#pane_item').children('.c').children().children('.c').children('.bti1').each(function (i, e) {
+			let obj = {};
+			obj.id = Number($(this).children('.bti2').children().children().text());
+			if ($(this).children('.bti3').children().length == 0) {
+				return true;
+			}
+			obj.name = $(this).children('.bti3').children().children().children().text();
+			obj.available = $(this).children('.bti3').children().attr('onclick') != undefined;
+			hvauto.battle.item[obj.id] = obj;
+		});
 		hvauto.battle.monster = [];
-		$('#pane_monster').children('.btm1').each(function(i, e) {
+		$('#pane_monster').children('.btm1').each(function (i, e) {
 			let obj = {};
 			obj.level = $(this).children('.btm2').children('div').children('div').filter('.fac').children('div').text();
 			obj.name = $(this).children('.btm3').children('div').filter('.fal').children('div').text();
-			$(this).children('.btm4').children('.btm5').children('.chbd').children('img').each(function(i, e) {
+			$(this).children('.btm4').children('.btm5').children('.chbd').children('img').each(function (i, e) {
 				if ($(this).attr('alt') != null) {
 					obj[$(this).attr('alt')] = /width:(\d+)px/.exec($(this).attr('style'))[1];
 				}
 			});
+			obj.isboss = $(this).attr('style') != undefined;
 			obj.effect = [];
-			$(this).children('.btm6').children('img').each(function(i, e) {
+			$(this).children('.btm6').children('img').each(function (i, e) {
 				obj.effect.push(hvauto.parseEffectScript($(this).attr('onmouseover')));
 			});
 			obj.alive = obj.health != undefined;
 			hvauto.battle.monster.push(obj);
 		});
 		hvauto.battle.log = [];
-		$('#textlog td').each(function(i, e) {
+		$('#textlog td').each(function (i, e) {
 			let text = $(this).text();
 			hvauto.battle.log.push(text);
-			let mat = /^Initializing .+\(Round ([0-9]+) \/ ([0-9]+)\)$/.exec(text); // seems not work yet
+			let mat = /^Initializing .+\(Round ([0-9]+) \/ ([0-9]+)\) ...$/.exec(text); // seems not work yet
+			hvauto.battle.round = {};
 			if (mat != null) {
 				hvauto.battle.round.current = Number(mat[1]);
 				hvauto.battle.round.all = Number(mat[2]);
-				console.log('Locate round infomation ' + hvauto.battle.round.current + ' / ' + hvauto.battle.round.all);
 			}
 		});
 		hvauto.battle.log.reverse();
@@ -163,7 +189,6 @@ hvauto.init = async function (cookie) {
 		console.log('Riddle master appear!!! Open your browser and finish it.');
 		return false;
 	} else {
-		console.log('You are not in a battle');
 		return false;
 	}
 };
@@ -190,48 +215,238 @@ hvauto.triggerSkill = function (target, skill) {
 	};
 };
 
+hvauto.itemInfo = {
+	'Health Gem': {
+		'id': 10005,
+		'desc': 'This powerup will restore a large amount of health.'
+	},
+	'Mana Gem': {
+		'id': 10006,
+		'desc': 'This powerup will restore a moderate amount of mana.'
+	},
+	'Spirit Gem': {
+		'id': 10007,
+		'desc': 'This powerup will restore a small amount of spirit.'
+	},
+	'Mystic Gem': {
+		'id': 10008,
+		'desc': 'This powerup will grant you the Channeling effect.'
+	},
+	'Health Draught': {
+		'id': 11191,
+		'desc': 'Provides a long-lasting health restoration effect.'
+	},
+	'Health Potion': {
+		'id': 11195,
+		'desc': 'Instantly restores a large amount of health.'
+	},
+	'Health Elixir': {
+		'id': 11199,
+		'desc': 'Fully restores health, and grants a long-lasting health restoration effect.'
+	},
+	'Mana Draught': {
+		'id': 11291,
+		'desc': 'Provides a long-lasting mana restoration effect.'
+	},
+	'Mana Potion': {
+		'id': 11295,
+		'desc': 'Instantly restores a moderate amount of mana.'
+	},
+	'Mana Elixir': {
+		'id': 11299,
+		'desc': 'Fully restores mana, and grants a long-lasting mana restoration effect.'
+	},
+	'Spirit Draught': {
+		'id': 11391,
+		'desc': 'Provides a long-lasting spirit restoration effect.'
+	},
+	'Spirit Potion': {
+		'id': 11395,
+		'desc': 'Instantly restores a moderate amount of spirit.'
+	},
+	'Spirit Elixir': {
+		'id': 11399,
+		'desc': 'Fully restores spirit, and grants a long-lasting spirit restoration effect.'
+	},
+	'Energy Drink': {
+		'id': 11401,
+		'desc': 'Restores 10 points of Stamina, up to the maximum of 99. When used in battle, also boosts Overcharge and Spirit by 10% for ten turns.'
+	},
+	'Caffeinated Candy': {
+		'id': 11402,
+		'desc': 'Restores 5 points of Stamina, up to the maximum of 99. When used in battle, also boosts Overcharge and Spirit by 10% for five turns.'
+	},
+	'Last Elixir': {
+		'id': 11501,
+		'desc': 'Fully restores all vitals, and grants long-lasting restoration effects.'
+	},
+	'Infusion of Flames': {
+		'id': 12101,
+		'desc': 'You gain +25% resistance to Fire elemental attacks and do 25% more damage with Fire magicks.'
+	},
+	'Infusion of Frost': {
+		'id': 12201,
+		'desc': 'You gain +25% resistance to Cold elemental attacks and do 25% more damage with Cold magicks.'
+	},
+	'Infusion of Lightning': {
+		'id': 12301,
+		'desc': 'You gain +25% resistance to Elec elemental attacks and do 25% more damage with Elec magicks.'
+	},
+	'Infusion of Storms': {
+		'id': 12401,
+		'desc': 'You gain +25% resistance to Wind elemental attacks and do 25% more damage with Wind magicks.'
+	},
+	'Infusion of Divinity': {
+		'id': 12501,
+		'desc': 'You gain +25% resistance to Holy elemental attacks and do 25% more damage with Holy magicks.'
+	},
+	'Infusion of Darkness': {
+		'id': 12601,
+		'desc': 'You gain +25% resistance to Dark elemental attacks and do 25% more damage with Dark magicks.'
+	},
+	'Scroll of Swiftness': {
+		'id': 13101,
+		'desc': 'Grants the Haste effect.'
+	},
+	'Scroll of Protection': {
+		'id': 13111,
+		'desc': 'Grants the Protection effect.'
+	},
+	'Scroll of the Avatar': {
+		'id': 13199,
+		'desc': 'Grants the Haste and Protection effects with twice the normal duration.'
+	},
+	'Scroll of Absorption': {
+		'id': 13201,
+		'desc': 'Grants the Absorb effect.'
+	},
+	'Scroll of Shadows': {
+		'id': 13211,
+		'desc': 'Grants the Shadow Veil effect.'
+	},
+	'Scroll of Life': {
+		'id': 13221,
+		'desc': 'Grants the Spark of Life effect.'
+	},
+	'Scroll of the Gods': {
+		'id': 13299,
+		'desc': 'Grants the Absorb, Shadow Veil and Spark of Life effects with twice the normal duration.'
+	},
+	'Flower Vase': {
+		'id': 19111,
+		'desc': 'There are three flowers in a vase. The third flower is green.'
+	},
+	'Bubble-Gum': {
+		'id': 19131,
+		'desc': 'It is time to kick ass and chew bubble-gum... and here is some gum.'
+	}
+};
+
+hvauto.useItem = function (target, item) { // maybe scrolls will use target.
+	return {
+		'method': 'action',
+		'mode': 'items',
+		'skill': item,
+		'target': target,
+		'token': hvauto.battle.token,
+		'type': 'battle'
+	};
+};
+
+hvauto.findEffect = function (name) {
+	let target = null;
+	hvauto.battle.effect.forEach(t => {
+		if (t.name == name) {
+			target = t;
+			return false;
+		}
+	});
+	return target;
+}
+
+hvauto.findItem = function (name) {
+	for (let key in hvauto.battle.item) {
+		if (hvauto.battle.item[key].name == name) {
+			return key;
+		}
+	}
+	return -1;
+};
+
 hvauto.doAction = async function (obj) {
 	let res = await hvauto.requestJson(obj);
-	let $ = cheerio.load(res['pane_effects']);
+	let $ = null;
+
+	$ = cheerio.load(res['pane_effects']);
 	hvauto.battle.effect = [];
-	$('img').each(function(i, e) {
+	$('img').each(function (i, e) {
 		hvauto.battle.effect.push(hvauto.parseEffectScript($(this).attr('onmouseover')));
 	});
-	
+
 	$ = cheerio.load(res['pane_vitals']);
-	hvauto.battle.health = $('#vrhd').text();
+	if ($('#vrhb').length > 0) {
+		hvauto.battle.health = $('#vrhb').text();
+	} else {
+		hvauto.battle.health = $('#vrhd').text(); // This seems used when Spark of Life is active
+	}
 	hvauto.battle.mana = $('#vrm').text();
 	hvauto.battle.spirit = $('#vrs').text();
 
-	hvauto.battle.skills = {}; // both skills and magic are in it
+	hvauto.battle.skills = {}; // both skills and magic are in i
 	$ = cheerio.load('<table>' + res['table_skills'] + '</table>');
-	$('.bts').children('div').each(function(i, e) {
+	$('.bts').children('div').each(function (i, e) {
 		let obj = hvauto.parseSpellScript($(this).attr('onmouseover'));
 		obj.id = Number($(this).attr('id'));
 		obj.available = $(this).attr('style') != 'opacity:0.5';
 		hvauto.battle.skills[obj.name] = obj;
 	});
+	
 	$ = cheerio.load('<table>' + res['table_magic'] + '</table>');
-	$('.bts').children('div').each(function(i, e) {
+	$('.bts').children('div').each(function (i, e) {
 		let obj = hvauto.parseSpellScript($(this).attr('onmouseover'));
 		obj.id = Number($(this).attr('id'));
 		obj.available = $(this).attr('style') != 'opacity:0.5';
 		hvauto.battle.skills[obj.name] = obj;
 	});
 
+	if (res['pane_item']) {
+		hvauto.battle.item = {};
+		$ = cheerio.load('<div id="root">' + res['pane_item'] + '</div>'); // Introduce a root
+
+		if ($('#root').children('.bti1').children('.bti3').children().length > 1) {
+			let obj = {
+				'id': 999
+			};
+			let name = $('#root').children('.bti1').children('.bti3').children().children().children().text();
+			obj.name = name;
+			obj.available = $('#root').children('.bti1').children('.bti3').children().attr('onclick') != undefined;
+			hvauto.battle.item[999] = obj;
+		}
+		$('#root').children('.c').children().children('.c').children('.bti1').each(function (i, e) {
+			let obj = {};
+			obj.id = Number($(this).children('.bti2').children().children().text());
+			if ($(this).children('.bti3').children().length == 0) {
+				return true;
+			}
+			obj.name = $(this).children('.bti3').children().children().children().text();
+			obj.available = $(this).children('.bti3').children().attr('onclick') != undefined;
+			hvauto.battle.item[obj.id] = obj;
+		});
+	}
+
 	$ = cheerio.load(res['pane_monster']);
 	hvauto.battle.monster = [];
-	$('.btm1').each(function(i, e) {
+	$('.btm1').each(function (i, e) {
 		let obj = {};
 		obj.level = $(this).children('.btm2').children('div').children('div').filter('.fac').children('div').text();
 		obj.name = $(this).children('.btm3').children('div').filter('.fal').children('div').text();
-		$(this).children('.btm4').children('.btm5').children('.chbd').children('img').each(function(i, e) {
+		$(this).children('.btm4').children('.btm5').children('.chbd').children('img').each(function (i, e) {
 			if ($(this).attr('alt') != null) {
 				obj[$(this).attr('alt')] = /width:(\d+)px/.exec($(this).attr('style'))[1];
 			}
 		});
 		obj.effect = [];
-		$(this).children('.btm6').children('img').each(function(i, e) {
+		$(this).children('.btm6').children('img').each(function (i, e) {
 			obj.effect.push(hvauto.parseEffectScript($(this).attr('onmouseover')));
 		});
 		obj.alive = obj.health != undefined;
@@ -245,7 +460,7 @@ hvauto.doAction = async function (obj) {
 			hvauto.handleLog(text);
 		}
 	});
-	
+
 	let stillRunning = false;
 	hvauto.battle.monster.forEach(t => {
 		if (t.alive) {
--- main.js
+++ main.js
@@ -5,27 +5,64 @@ let hvauto = require('./hvauto');
 
 async function main() {
 	hvauto.handleLog = t => {
-		console.log('Log: ' + t);
+		console.log('\t' + t);
 	};
 	while (await hvauto.init(cookie)) { // Auto load next battle
-		console.log('You are in a battle');
+		let battle = hvauto.battle;
+		if (battle.round.current) {
+			console.log('Round ' + battle.round.current + ' / ' + battle.round.all);
+		}
 		let nextAction = null;
 		do {
-			console.log('Health: ' + hvauto.battle.health);
+			console.log('Health: ' + battle.health + ', Mana: ' + battle.mana + ', Spirit: ' + battle.spirit + ', Charge: ' + battle.charge);
 			let target = 0;
-			for (let i = 0; i < hvauto.battle.monster.length; ++i) {
-				if (hvauto.battle.monster[i].alive) {
+			for (let i = 0; i < battle.monster.length; ++i) {
+				if (battle.monster[i].alive) {
 					target = i + 1;
 					break;
 				}
 			}
-			if (hvauto.battle.health < 4000) {
-				console.log('Cast regen');
-				nextAction = hvauto.triggerSkill(0, hvauto.battle.skills.Regen.id);
-			} else {
-				console.log('Normat attack ' + target);
-				nextAction = hvauto.normalAttack(target);
+			if (battle.item[999]) {
+				if (battle.item[999].name == 'Mana Gem' && battle.mana < 300) {
+					nextAction = hvauto.useItem(0, '999');
+					continue;
+				} else if (battle.item[999].name == 'Health Gem' && battle.health < 4500) {
+					nextAction = hvauto.useItem(0, '999');
+					continue;
+				}
 			}
+			if (battle.health < 4000) {
+				if (hvauto.findEffect('Regen') == null && battle.skills.Regen) {
+					if (battle.mana >= battle.skills.Regen.mana && battle.skills.Regen.available) {
+						console.log('Cast regen');
+						nextAction = hvauto.triggerSkill(0, battle.skills.Regen.id);
+						continue;
+					}
+				}
+			}
+			if (battle.health < 2000) {
+				if (battle.skills.Cure) {
+					if (battle.mana >= battle.skills.Cure.mana && battle.skills.Cure.available) {
+						console.log('Cast cure');
+						nextAction = hvauto.triggerSkill(0, battle.skills.Cure.id);
+						continue;
+					}
+				}
+			}
+			if (battle.mana < 100 && hvauto.findEffect('Replenishment') == null) {
+				let pos = hvauto.findItem('Mana Draught');
+				if (pos == -1) {
+					console.log('You don\'t take mana draught.');
+				} else {
+					if (battle.item[pos].available) {
+						console.log('Use mana draught');
+						nextAction = hvauto.useItem(0, pos);
+						continue;
+					}
+				}
+			}
+			console.log('Normat attack ' + target);
+			nextAction = hvauto.normalAttack(target);
 		} while (await hvauto.doAction(nextAction));
 	}
 }

c487e0912c0a449577fbdf282ded7188e03b4efd
nekosu <liao.hy@outlook.com>
2020-07-26 00:49:33 +0800

    Fix wrong updating

--- updater.js
+++ updater.js
@@ -3,15 +3,18 @@ const got = require('got');
 
 module.exports = async () => {
 	let obj = JSON.parse(fs.readFileSync('updater.json'));
+	let pros = [];
 	for (let i = 0; i < obj.file.length; ++i) {
-		let p = obj.file[i];
-		let res = await got(obj.root + '/' + p);
-		fs.writeFile(p, res.body, err => {
-			if (err) {
-				console.log('Updating ' + p + ' failed: ' + err);
-			} else {
-				console.log('Updating ' + p + ' success');
-			}
-		});
+		(() => {
+			let path = obj.file[i];
+			pros.push(got(obj.root + '/' + path).then(res => {
+				console.log('Update ' + path + ' success');
+				return res.body;
+			}));
+		})();
 	}
-}
+	let data = await Promise.all(pros);
+	for (let i = 0; i < obj.file.length; ++i) {
+		fs.writeFileSync(obj.file[i], data[i]);
+	}
+};

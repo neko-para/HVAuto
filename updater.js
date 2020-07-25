d6d6be23c1aae508c288ba2b44feb0b3092b8e4c
nekosu <liao.hy@outlook.com>
2020-07-25 21:15:25 +0800

    Fix updater async

--- updater.js
+++ updater.js
@@ -3,9 +3,9 @@ const got = require('got');
 
 module.exports = async () => {
 	let obj = JSON.parse(fs.readFileSync('updater.json'));
-	obj.file.forEach(async (p) => {
-		let url = obj.root + '/' + p;
-		let res = await got(url);
+	for (let i = 0; i < obj.file.length; ++i) {
+		let p = obj.file[i];
+		let res = await got(obj.root + '/' + p);
 		fs.writeFile(p, res.body, err => {
 			if (err) {
 				console.log('Updating ' + p + ' failed: ' + err);
@@ -13,6 +13,5 @@ module.exports = async () => {
 				console.log('Updating ' + p + ' success');
 			}
 		});
-	});
-
+	}
 }
